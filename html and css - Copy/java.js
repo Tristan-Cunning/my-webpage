531ee23032c378642fb5ede5f00fd43bc68f3852
Tristan Cunningham <firstahsgcd2016@gmail.com>
2025-12-12 18:08:12 -0700

    Delete html and css - Copy directory

--- html and css - Copy/java.js
+++
@@ -1,5 +0,0 @@
-function updateUrgency() {
-    const newValue = document.querySelector('.js_urgency').value;
-    document.querySelector('.js_urgency_value').value = newValue;
-}
-document.querySelector('.js_urgency').addEventListener('change', updateUrgency);
